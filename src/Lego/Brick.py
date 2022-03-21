ffc81559d5b694be9e413762c8eae2d5b9ebfe69
Théo Weimann <theo.weimann.priv@gmail.com>
2022-03-20 21:14:35 -0400

    feat: Add basic type prototype to model a lego set
    Add basic type prototype to model a lego set such ass Brick and Sets. To
    be completed later

---
+++ src/Lego/Brick.py
@@ -0,0 +1,4 @@
+class Brick:
+    def __init__(self, name, reference):
+        self.name = name
+        self.reference = reference
---
+++ src/Lego/Set.py
@@ -0,0 +1,8 @@
+from Lego.Brick import Brick
+
+
+class Set:
+    def __init__(self, bricks: list[Brick], ):
+        self.bricks = bricks
+
+# TODO : Convert this to json : https://brickset.com/inventories/31203-1
